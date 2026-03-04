689f3ba22e55829b152f4c904e436ae6752bc157
michaelzuluaga04 <michael.zuluaga@globant.com>
2026-03-04 10:27:27 -0500

    Agrego clase perrito de prueba 02

--- Perrito.java
+++
@@ -1,9 +0,0 @@
-public class Perrito {
-    private String nombre;
-    public Perrito(String nombre) {
-        this.nombre = nombre;
-    }
-    public void saludar() {
-        System.out.println("Hola, humano soy " + nombre);
-    }
-}
--- Persona.java
+++
@@ -1,9 +0,0 @@
-public class Persona {
-    private String nombre;
-    public Persona(String nombre) {
-        this.nombre = nombre;
-    }
-    public void saludar() {
-        System.out.println("Hola, soy " + nombre);
-    }
-}
